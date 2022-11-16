e5da9619d75467a9b0fc3960c84f055308f51e4c
xxkl1 <lmmonster@aliyun.com>
2022-11-16 20:00:21 +0800

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+import hyperid from 'hyperid';
+
+let a = Buffer.from('uuidPart' + '==', 'base64')
+console.log(a)
---
+++ main.js
@@ -0,0 +1,29 @@
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import { setupCounter } from './counter.js';
+import hyperid from 'hyperid';
+
+console.log('hyperid:', hyperid);
+const uuid = hyperid();
+console.log('uuid5:', uuid)
+console.log('uuid:', uuid());
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector('#counter'));
